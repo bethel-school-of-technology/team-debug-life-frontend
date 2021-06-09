fa7257b13c954c0753abc9bbec409c8a3881094e
Dan Helgeson <danhelgeson@gmail.com>
2021-06-09 11:11:31 -0500

    Cleaning up main branch

--- debuglife-frontend/src/App.js
+++ debuglife-frontend/src/App.js
@@ -26,53 +26,53 @@ export default App;
 
 
 
-// import React, { Component } from "react";
-// import { reduxForm, Field } from "redux-form";
+import React, { Component } from "react";
+import { reduxForm, Field } from "redux-form";
 
-// import { FormTitle } from "../formTitle";
-// import { FormInput, FormButton } from "../formFields";
-// import TextLink from "../textLink";
+import { FormTitle } from "../formTitle";
+import { FormInput, FormButton } from "../formFields";
+import TextLink from "../textLink";
 
-// class SignupForm extends Component {
-//   render() {
-//     return (
-//       <form className="sign-up-form">
-//         <FormTitle className="sign-up-form__title" text="Login" />
-//         <Field
-//           className="sign-up-form__email"
-//           placeholder="Enter Email"
-//           name="email"
-//           type="email"
-//           title="Email"
-//           component={FormInput}
-//         />
-//         <Field
-//           className="sign-up-form__password"
-//           placeholder="Enter Password"
-//           name="password"
-//           type="password"
-//           title="Password"
-//           component={FormInput}
-//         />
-//         <Field
-//           className="sign-up-form__login"
-//           name="login"
-//           type="submit"
-//           title="Login"
-//           component={FormButton}
-//         />
-//         <div className='sign-up-form__text-links'>
-//             <TextLink to='/forgot' text='Forgot Password'/>
-//             <TextLink to='/signup' text='Not a member? Register here'/>
-//         </div>
+class SignupForm extends Component {
+  render() {
+    return (
+      <form className="sign-up-form">
+        <FormTitle className="sign-up-form__title" text="Login" />
+        <Field
+          className="sign-up-form__email"
+          placeholder="Enter Email"
+          name="email"
+          type="email"
+          title="Email"
+          component={FormInput}
+        />
+        <Field
+          className="sign-up-form__password"
+          placeholder="Enter Password"
+          name="password"
+          type="password"
+          title="Password"
+          component={FormInput}
+        />
+        <Field
+          className="sign-up-form__login"
+          name="login"
+          type="submit"
+          title="Login"
+          component={FormButton}
+        />
+        <div className='sign-up-form__text-links'>
+            <TextLink to='/forgot' text='Forgot Password'/>
+            <TextLink to='/signup' text='Not a member? Register here'/>
+        </div>
 
-//       </form>
-//     );
-//   }
-// }
+      </form>
+    );
+  }
+}
 
-// SignupForm = reduxForm({
-//   form: "signup"
-// })(SignupForm);
+SignupForm = reduxForm({
+  form: "signup"
+})(SignupForm);
 
-// export default SignupForm;
+export default SignupForm;
---
+++ debuglife-frontend/src/Index.js
@@ -0,0 +1,2 @@
+// const { null } = require("check-types");
+
