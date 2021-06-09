23c78fa9000b293b0107b69992ecc580ff1203a8
Dan Helgeson <danhelgeson@gmail.com>
2021-06-08 21:12:04 -0500

    Clean Slate

--- debuglife-frontend/src/App.js
+++ debuglife-frontend/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
@@ -23,3 +23,56 @@ function App() {
 }
 
 export default App;
+
+
+
+// import React, { Component } from "react";
+// import { reduxForm, Field } from "redux-form";
+
+// import { FormTitle } from "../formTitle";
+// import { FormInput, FormButton } from "../formFields";
+// import TextLink from "../textLink";
+
+// class SignupForm extends Component {
+//   render() {
+//     return (
+//       <form className="sign-up-form">
+//         <FormTitle className="sign-up-form__title" text="Login" />
+//         <Field
+//           className="sign-up-form__email"
+//           placeholder="Enter Email"
+//           name="email"
+//           type="email"
+//           title="Email"
+//           component={FormInput}
+//         />
+//         <Field
+//           className="sign-up-form__password"
+//           placeholder="Enter Password"
+//           name="password"
+//           type="password"
+//           title="Password"
+//           component={FormInput}
+//         />
+//         <Field
+//           className="sign-up-form__login"
+//           name="login"
+//           type="submit"
+//           title="Login"
+//           component={FormButton}
+//         />
+//         <div className='sign-up-form__text-links'>
+//             <TextLink to='/forgot' text='Forgot Password'/>
+//             <TextLink to='/signup' text='Not a member? Register here'/>
+//         </div>
+
+//       </form>
+//     );
+//   }
+// }
+
+// SignupForm = reduxForm({
+//   form: "signup"
+// })(SignupForm);
+
+// export default SignupForm;
