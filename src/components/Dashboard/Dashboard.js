818af78b3b56f9ab853c40d37a3e2f45a5ff5257
Dan Helgeson <danhelgeson@gmail.com>
2021-06-17 23:38:37 -0500

    Added basic About Game & Game Instructions sections to Dashboard/GamePage

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -4,8 +4,13 @@ import './Dashboard.css';
 export default function Dashboard() {
     return(
         <div className='dashboard-wrapper'>
-            <h2>Dreaming Dutchman's Spooky Escape</h2>
-            <p>Help Captain Roger "Salty Dog" Sacks break free from The Serpent of the Seas</p>
-        </div>        
+            <h1>Dreaming Dutchman's Spooky Escape</h1>
+            <p>Help Captain "Salty Dog" Rogers break free from The Serpent of the Seas</p>
+           
+            <h2>About Game</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <h2>Game Instructions</h2>
+            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>
+        </div>   
     );
 }
