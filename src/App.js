e853b853b2b5d0a7286dff78aa76ed128d393b11
Rashaan Daniel Brandon <rayray.brandon@gmail.com>
2021-06-25 14:50:49 -0400

    updated app.js

--- src/App.js
+++ src/App.js
@@ -16,27 +16,20 @@ function App() {
           <Route path='/login'>
             <Login setToken={setToken} />
           </Route>
-          <Route path='/dashboard'>
-            <Dashboard />
-          </Route>
-          <Route path='/preferences'>
-            <Preferences />
-            </Route>
           <Route path = '/CreateAccount'>
             <CreateAccount />
           </Route>
-<<<<<<< HEAD
-=======
           <Route path='/game'>
             <GamePage userToken={token} />
           </Route>
           <Redirect from='/' to="/game" />
->>>>>>> 15bf85b48dc0d1b410382bc122645ce327ddb27e
         </Switch>
       </BrowserRouter>
     </div>
   ); 
 }
+
+export default App;
 
 export default App;
 
