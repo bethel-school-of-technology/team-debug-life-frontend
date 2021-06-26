b8e08c4467bfb4bb7cd4d8d8f36308c96c1d0266
Rashaan Daniel Brandon <rayray.brandon@gmail.com>
2021-06-26 18:41:11 -0400

    took out usestate

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import React from 'react'
+import React from 'react';
 import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 import './App.css';
 import GamePage from './components/Game/GamePage.js';
 import Login from './components/Login/Login.js';
 import useToken from './useToken';
 import CreateAccount from './components/Create-account/CreateAccount.js';
-
 function App() {
   const { token, setToken } = useToken();
 
