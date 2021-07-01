85933ca133c2a380fbd71f1a33df2bd4f9bd304d
Rashaan Daniel Brandon <rayray.brandon@gmail.com>
2021-07-01 16:35:21 -0400

    saved progress

--- src/App.js
+++ src/App.js
@@ -3,13 +3,9 @@ import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 import './App.css';
 import GamePage from './components/Game/GamePage.js';
 import useToken from './useToken';
-<<<<<<< HEAD
-import CreateAccount from './components/Create-account/CreateAccount.js';
-=======
 import Login from './components/Forms/Login/Login.js';
 import CreateAccount from './components/Forms/Create-account/CreateAccount.js';
 
->>>>>>> ec48a32b9bd813a769ad12b33ef2c4d2ebf00543
 function App() {
   const { token, setToken } = useToken();
 
