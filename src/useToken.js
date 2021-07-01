bac80c1f13ebaf66fdb7ee1181042dd5ecf2cd66
Grant <nerfboyforever@gmail.com>
2021-07-01 16:10:58 -0500

    SetToken now given proper token value

--- src/useToken.js
+++ src/useToken.js
@@ -4,18 +4,17 @@ export default function useToken() {
   const getToken = () => {
     const tokenString = localStorage.getItem('token');
     const userToken = JSON.parse(tokenString);
-    console.log(userToken);
     return userToken;
   };
 
   const [token, setToken] = useState(getToken());
 
   const saveToken = userToken => {
-      if (!userToken) {
-          userToken = {'token': null}
-      }
+    if (!userToken) {
+        userToken = {'token': null}
+    }
     localStorage.setItem('token', JSON.stringify(userToken));
-    setToken(userToken.token);
+    setToken(userToken);
   };
 
   return {
